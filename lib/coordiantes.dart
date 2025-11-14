45e731367d5cc0c6b9728a6dbfe6a5b3759f7122
mohamed <mohamedkha474@gmail.com>
2025-11-14 19:20:03 +0200

    delete comment

--- lib/coordiantes.dart
+++ lib/coordiantes.dart
@@ -1,43 +1,8 @@
-// // models/coordiantes.dart
-// class Coordiantes {
-//   final double latitude;
-//   final double longitude;
-//   final String placeName;
-//   final double? distanceKm; // المسافة من مرجعية (كم)
-//   final bool visited;       // أثناء بناء المسار
-//   final int? order;         // ترتيب الزيارة في المسار النهائي
-
-//   Coordiantes({
-//     required this.placeName,
-//     required this.latitude,
-//     required this.longitude,
-//     this.distanceKm,
-//     this.visited = false,
-//     this.order,
-//   });
-
-//   Coordiantes copyWith({
-//     double? distanceKm,
-//     bool? visited,
-//     int? order,
-//   }) {
-//     return Coordiantes(
-//       placeName: placeName,
-//       latitude: latitude,
-//       longitude: longitude,
-//       distanceKm: distanceKm ?? this.distanceKm,
-//       visited: visited ?? this.visited,
-//       order: order ?? this.order,
-//     );
-//   }
-// }
-
-
 class Coordiantes {
   final double latitude;
   final double longitude;
   final String placeName;
-  double? distanceKm; // nullable وممكن تتحدَّث
+  double? distanceKm;
 
   Coordiantes({
     required this.placeName,
